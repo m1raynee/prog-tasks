18fe7a62a32c9cf327674a609b7ced31d05fe467
miray <71838742+m1raynee@users.noreply.github.com>
2024-11-07 18:00:20 +0300

    5. out2 tweaks

--- tasks/5/5.cpp
+++ tasks/5/5.cpp
@@ -20,11 +20,11 @@ int main() {
     char name[100] = "C:/Users/peche/OneDrive/Документы/Github/prog-tasks/tasks/5/0.txt";
 
     ifstream f;
-    ofstream result;
+    ofstream result_stream;
     f.open(name);
-    result.open("result.txt");
+    result_stream.open("result.txt");
 
-    if (!(f.is_open() and result.is_open())) {
+    if (!(f.is_open() and result_stream.is_open())) {
         cout << "ERR: Файл не открылся\n";
         return 0;
     }
@@ -47,13 +47,15 @@ int main() {
     f.close();
 
     Out1(A, N, N, &cout);
-    Out1(A, N, N, &result);
+    Out1(A, N, N, &result_stream);
 
-    Out2(Process(A, N, N, mk), mk, &cout);
-    Out2(Process(A, N, N, mk), mk, &result);
+    float result_value = Process(A, N, N, mk);
+    Out2(result_value, mk, &cout);
+    Out2(result_value, mk, &result_stream);
 
     return 0;
 }
+
 
 void __OutAi(ostream *out, float *A, uint size_b) {
     for (uint i = 0; i < size_b; i++) {*out << A[i] << "\t|";}
@@ -76,8 +78,17 @@ void Out1(float **A, uint size_a, uint size_b, ostream *out) {
 }
 
 void Out2(float result, int *mk, ostream *out) {
-    *out << "Максимальное число в заданной области: " << result << '\n'
-    << "m=" << mk[0] << " k=" << mk[1] << '\n';
+    *out << "Максимальное число в заданной области";
+
+    if (result == -INFINITY) {
+        *out << " не найдено, вероятно (на самом деле никак по-другому это не получится)\n"
+        << "области выходят за размер матрицы";
+    }
+    else {
+        *out << ": " << result;
+    }
+
+    *out << "\nm=" << mk[0] << " k=" << mk[1] << '\n';
 }
 
 int *ReadMK(istream *in) {
@@ -151,9 +162,9 @@ float **ReadFile(istream *in, uint size_a, uint size_b) {
 }
 
 float Process(float **matrix, uint size_a, uint size_b, int *mk) {
-    static float max_value = -HUGE_VALF;
-    for (uint i = 0; i < size_a; i++) {
-        for (uint j = 0; j < size_b; j++) {
+    static float max_value = -INFINITY;
+    for (int i = 0; i < size_a; i++) {
+        for (int j = 0; j < size_b; j++) {
             if (
                 (
                     (i >= mk[0] and i <= mk[1]) 
