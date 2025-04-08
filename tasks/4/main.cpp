c56d3ceecd936ced4a1169fd63a900cb18bef9b3
miray <71838742+m1raynee@users.noreply.github.com>
2025-04-08 15:37:23 +0300

    4 add out

--- tasks/4/main.cpp
+++ tasks/4/main.cpp
@@ -4,14 +4,16 @@
 
 int main() {
     std::ifstream in1, in2;
+    std::ofstream out;
     in1.open("in.txt");
     in2.open("in copy.txt");
+    out.open("out.txt");
     strings l1 = readfile(in1);
     strings l2 = readfile(in2);
-    l1.print(std::cout);
-    bar(std::cout);
-    l2.print(std::cout);
-    bar(std::cout);
+    l1.print(out);
+    bar(out);
+    l2.print(out);
+    bar(out);
 
     strings::Node *p = l2.first, *q = l2.first->next;
     while (p) {
@@ -27,6 +29,6 @@ int main() {
         if (q) q = q->next;
     }
     l1.last->next = l2.cur;
-    l1.print(std::cout);
+    l1.print(out);
     return 0;
 }
